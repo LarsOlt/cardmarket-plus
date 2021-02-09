a235960f6d2726c957ff5f91665c6ca352d0ae52
Lars <larsoeltjenbruns@gmail.com>
2021-02-09 15:08:14 +0100

    add scss support

--- libs/hot-reload.js
+++ libs/hot-reload.js
@@ -29,10 +29,10 @@ const watchChanges = (dir, lastTimestamp) => {
 chrome.management.getSelf (self => {
     if (self.installType === 'development') {
         chrome.runtime.getPackageDirectoryEntry (dir => watchChanges (dir))
-        chrome.tabs.query ({ active: true, lastFocusedWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
-            if (tabs[0]) {
-                chrome.tabs.reload (tabs[0].id)
-            }
-        })
+        // chrome.tabs.query ({ active: true, lastFocusedWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
+        //     if (tabs[0]) {
+        //         chrome.tabs.reload (tabs[0].id)
+        //     }
+        // })
     }
 })
